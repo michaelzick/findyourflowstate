e78082619017aaf2361840c1a2491bdac688cded
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2025-08-28 17:26:59 -0700

    flipped mobile psych img

--- src/components/quiz/QuizLanding.tsx
+++ src/components/quiz/QuizLanding.tsx
@@ -199,7 +199,7 @@ export function QuizLanding() {
         {/* Mobile Background - Single Image */}
         <div className="absolute inset-0 md:hidden">
           <div
-            className="absolute inset-0 bg-cover bg-center opacity-60"
+            className="absolute inset-0 bg-cover bg-center opacity-60 transform scale-x-[-1]"
             style={{ backgroundImage: `url(${psychologistImage})` }}
           />
           <div className="absolute inset-0 bg-background/70" />
